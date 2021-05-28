464beb2eaf5144b0abccba7f20e045d80490c909
Jesnine Erillo <jesnineerillo@berkeley.edu>
2021-05-27 17:52:06 -0700

    Improved GUI

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ import json
 
 from flask import Flask, render_template, request, redirect
 
+def meters_to_miles(meters):
+        return meters / 1609
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('form.html')
 
-@app.route('/handle_data', methods=['POST'])
+@app.route('/get_random_business', methods=['POST'])
 def handle_data():
     form = request.form.to_dict()
     location = form['location']
     print('Search form values: ', form)
-    res = get_random_business(form)
-    display_business(res)
-    return """
-    <a href="{}" target="_blank">{}</a>
-    <button onclick="location.href='/'" type="button">
-         Randomize again</button>
-    """.format(res['url'], res['name'])
+    business = get_random_business(form)
+    if business:
+        # For Debugging
+        display_business(business)
+
+        business['location'] = ',\n'.join(x for x in business['location']['display_address'])
+        business['categories'] = ', '.join(str(x['title']) for x in business['categories'])
+        business['distance'] = '{:.2f} miles'.format(meters_to_miles(business['distance']))
+
+    return render_template('result.html', business=business)
 
 def get_random_business(params):
     search_url = 'https://api.yelp.com/v3/businesses/search'
@@ -31,10 +37,9 @@ def get_random_business(params):
     response = requests.get(search_url, headers=headers, params=params)
     try:
         business = random.choice(response.json()['businesses'])
+        return defaultdict(str, business)
     except:
-        return { 'name': 'No results found.' }
-    return defaultdict(str, business)
-
+        return None
 
 def display_business(business):
     """Displays relevant information associated with business.
@@ -44,9 +49,6 @@ def display_business(business):
     """
     def table_print(title, item):
         print('{:15} {}'.format(title + ':', item if item else 'Unknown'))
-
-    def meters_to_miles(meters):
-        return meters / 1609
 
     if type(business) is not defaultdict:
         return
