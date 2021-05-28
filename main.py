7551b65abd1b1a98775f9c4f51297c82d22498fc
Jesnine Erillo <jesnineerillo@berkeley.edu>
2021-05-27 20:33:29 -0700

    Optionally use environment variable for API_KEY

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import requests
 import random
 import json
+import os
 
 from flask import Flask, render_template, request, redirect
 
@@ -33,7 +34,7 @@ def handle_data():
 
 def get_random_business(params):
     search_url = 'https://api.yelp.com/v3/businesses/search'
-    headers = {'Authorization': 'Bearer {}'.format(API_KEY)}
+    headers = {'Authorization': 'Bearer {}'.format(os.environ.get('API_KEY',API_KEY))}
     response = requests.get(search_url, headers=headers, params=params)
     try:
         business = random.choice(response.json()['businesses'])
